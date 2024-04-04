68f2dc630d3b5cccd15936ec7bfe99163435e6e0
Shariful <sharifaiub15@gmail.com>
2024-04-05 00:08:23 +0200

    Assignment 01

---
+++ Introduction to Programming Language/Week 01 (Assignment 01)/Problem 02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int A, B;
+
+    scanf("%d %d", &A, &B);
+    printf("%lld", (long long)A*B);
+
+    return 0;
+}
