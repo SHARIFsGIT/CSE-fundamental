33d9bb8e9613aef4067b126220772895b2a7340c
Shariful <sharifaiub15@gmail.com>
2024-04-04 23:47:57 +0200

    Module 3.5

---
+++ Introduction to Programming Language/Week 01 (Practice day 02)/Practice problem 03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+    scanf("%d",&N);
+    for(int i=0; i<N; i++){
+        printf("I Love Practice\n");
+    }
+    return 0;
+}
