56f5542042841cc3a59136243f3f72f2c5f3f7ae
Shariful <sharifaiub15@gmail.com>
2024-06-30 23:27:00 +0200

    SLL/Vector palidrom/delete any value

---
+++ Basic data structures/Mod 07 (SLL Recap)/delete_specific_value.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void delete_value(Node *&head, int x)
+{
+    if (head->val == x)
+    {
+        head = head->next;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        if (tmp->next->val == x)
+        {
+            Node *deleteNode = tmp->next;
+            tmp->next = tmp->next->next;
+            delete deleteNode;
+        }
+
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+void print_linekd_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_tail(head, tail, val);
+    }
+
+    int x;
+    cin >> x;
+
+    delete_value(head, x);
+
+    print_linekd_list(head);
+
+    return 0;
+}
