4b08d2598d947b1d2839169b345bbe698b57415c
Shariful <sharifaiub15@gmail.com>
2024-07-08 00:08:43 +0200

    mid visit browser new solution

--- Basic data structures/Mode 12 (Mid exam)/Browser_History.cpp
+++ Basic data structures/Mode 12 (Mid exam)/Browser_History.cpp
@@ -1,5 +1,118 @@
-#include <iostream>
-#include <sstream>
+// #include <iostream>
+// #include <sstream>
+// using namespace std;
+
+// class Node
+// {
+// public:
+//     Node *previous;
+//     string address;
+//     Node *next;
+//     Node(string address)
+//     {
+//         this->previous = NULL;
+//         this->address = address;
+//         this->next = NULL;
+//     }
+// };
+
+// void insert_at_tail(Node *&head, Node *&tail, string address)
+// {
+//     Node *newNode = new Node(address);
+//     if (head == NULL)
+//     {
+//         head = newNode;
+//         tail = newNode;
+//         return;
+//     }
+//     tail->next = newNode;
+//     newNode->previous = tail;
+//     tail = newNode;
+// }
+
+// Node *find_node(Node *head, string address)
+// {
+//     Node *temp = head;
+//     while (temp != NULL)
+//     {
+//         if (temp->address == address)
+//         {
+//             return temp;
+//         }
+//         temp = temp->next;
+//     }
+//     return 0;
+// }
+
+// int main()
+// {
+//     Node *head = NULL;
+//     Node *tail = NULL;
+//     string address;
+
+//     while (cin >> address && address != "end")
+//     {
+//         insert_at_tail(head, tail, address);
+//     }
+
+//     Node *current = head;
+//     int q;
+//     cin >> q;
+//     cin.ignore();
+
+//     while (q--)
+//     {
+//         string str;
+//         getline(cin, str);
+
+//         stringstream ss(str);
+//         string word, addr;
+
+//         ss >> word;
+//         if (word == "visit")
+//         {
+//             ss >> addr;
+//             Node *foundNode = find_node(head, addr);
+//             if (foundNode)
+//             {
+//                 current = foundNode;
+//                 cout << current->address << endl;
+//             }
+//             else
+//             {
+//                 cout << "Not Available" << endl;
+//             }
+//         }
+//         else if (word == "next")
+//         {
+//             if (current->next)
+//             {
+//                 current = current->next;
+//                 cout << current->address << endl;
+//             }
+//             else
+//             {
+//                 cout << "Not Available" << endl;
+//             }
+//         }
+//         else if (word == "prev")
+//         {
+//             if (current->previous)
+//             {
+//                 current = current->previous;
+//                 cout << current->address << endl;
+//             }
+//             else
+//             {
+//                 cout << "Not Available" << endl;
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
 
 class Node
@@ -41,7 +154,47 @@ Node *find_node(Node *head, string address)
         }
         temp = temp->next;
     }
-    return 0;
+    return NULL;
+}
+
+void handle_visit(Node *&current, Node *head, string address)
+{
+    Node *foundNode = find_node(head, address);
+    if (foundNode)
+    {
+        current = foundNode;
+        cout << current->address << endl;
+    }
+    else
+    {
+        cout << "Not Available" << endl;
+    }
+}
+
+void handle_next(Node *&current)
+{
+    if (current->next)
+    {
+        current = current->next;
+        cout << current->address << endl;
+    }
+    else
+    {
+        cout << "Not Available" << endl;
+    }
+}
+
+void handle_prev(Node *&current)
+{
+    if (current->previous)
+    {
+        current = current->previous;
+        cout << current->address << endl;
+    }
+    else
+    {
+        cout << "Not Available" << endl;
+    }
 }
 
 int main()
@@ -62,50 +215,25 @@ int main()
 
     while (q--)
     {
-        string str;
-        getline(cin, str);
+        string command;
+        getline(cin, command);
 
-        stringstream ss(str);
-        string word, addr;
+        stringstream ss(command);
+        string cmd, addr;
 
-        ss >> word;
-        if (word == "visit")
+        ss >> cmd;
+        if (cmd == "visit")
         {
             ss >> addr;
-            Node *foundNode = find_node(head, addr);
-            if (foundNode)
-            {
-                current = foundNode;
-                cout << current->address << endl;
-            }
-            else
-            {
-                cout << "Not Available" << endl;
-            }
+            handle_visit(current, head, addr);
         }
-        else if (word == "next")
+        else if (cmd == "next")
         {
-            if (current->next)
-            {
-                current = current->next;
-                cout << current->address << endl;
-            }
-            else
-            {
-                cout << "Not Available" << endl;
-            }
+            handle_next(current);
         }
-        else if (word == "prev")
+        else if (cmd == "prev")
         {
-            if (current->previous)
-            {
-                current = current->previous;
-                cout << current->address << endl;
-            }
-            else
-            {
-                cout << "Not Available" << endl;
-            }
+            handle_prev(current);
         }
     }
 
