810ea5fd812ba0296ddb534c3ce4bf9187dffe43
Shariful <sharifaiub15@gmail.com>
2024-07-20 12:56:44 +0200

    tree implement

---
+++ Basic data structures/Tree basic/tree_basic.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void preOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->value << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void postOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->value << " ";
+}
+
+void inOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    inOrder(root->left);
+    cout << root->value << " ";
+    inOrder(root->right);
+}
+
+int main()
+{
+    Node *a = new Node(10);
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+    Node *e = new Node(50);
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    c->right = e;
+
+    preOrder(a);
+    cout << endl;
+    postOrder(a);
+    cout << endl;
+    inOrder(a);
+
+    return 0;
+}
