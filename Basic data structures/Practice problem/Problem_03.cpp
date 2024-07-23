66ad60ae230915ce4cd107c49de972d87ef18e16
Shariful <sharifaiub15@gmail.com>
2024-07-23 23:59:02 +0200

    vector/stack practice problem

--- Basic data structures/Practice problem/Problem_03.cpp
+++ Basic data structures/Practice problem/Problem_03.cpp
@@ -3,6 +3,45 @@ using namespace std;
 
 int main()
 {
-    
+    int q;
+    cin >> q;
+
+    vector<int> v;
+    for (int i = 0; i < q; i++)
+    {
+        int x, value;
+        cin >> x;
+
+        if (x == 1)
+        {
+            cin >> value;
+            if (!v.size())
+            {
+                v.push_back(value);
+            }
+            else
+            {
+                v.insert(v.begin() + 1, value);
+            }
+        }
+        else if (x == 2)
+        {
+            cin >> value;
+            if (!v.size())
+            {
+                v.push_back(value);
+            }
+            else
+            {
+                v.insert(v.end() - 1, value);
+            }
+        }
+    }
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
+    }
+
     return 0;
 }
---
+++ Basic data structures/Practice problem/Problem_04.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    stack<int> st;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        st.push(v[i]);
+    }
+
+    int k;
+    cin >> k;
+
+    for (int i = 0; i < k - 1; i++)
+    {
+        st.pop();
+    }
+    cout << st.top() << endl;
+
+    return 0;
+}
