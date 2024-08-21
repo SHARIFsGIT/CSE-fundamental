8378114d38b348fbab2aeeddafd32f880f4a209f
Shariful <sharifaiub15@gmail.com>
2024-08-21 07:20:13 +0200

    pre/post/in order

---
+++ Basic data structures/Mod 17 (Binary tree)/in_order.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *left;
+    int value;
+    Node *right;
+
+    Node(int value)
+    {
+        this->left = NULL;
+        this->value = value;
+        this->right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    preorder(root->left);
+    cout << root->value << " ";
+    preorder(root->right);
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    // Connections:
+    root->left = a;
+    root->right = b;
+
+    a->left = c;
+    a->right = h;
+
+    c->right = e;
+
+    b->right = d;
+
+    d->left = f;
+    d->right = g;
+
+    h->right = i;
+
+    // Function calls:
+    preorder(root);
+
+    return 0;
+}
