d52e139475f54320a2aa7172e316a1b307fadb7d
Shariful <sharifaiub15@gmail.com>
2024-08-21 23:37:42 +0200

    level order/tree input/count nodes/count leaf/ max hight of tree

---
+++ Basic data structures/Mod 18 (Binary tree operation)/level_order_implement.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *left;
+    int value;
+    Node *right;
+
+    Node(int value)
+    {
+        this->left = NULL;
+        this->value = value;
+        this->right = NULL;
+    }
+};
+
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree is empty" << endl;
+        return;
+    }
+
+    queue<Node *> queue;
+    queue.push(root);
+
+    while (!queue.empty())
+    {
+        // 1. Keep separate the front value of queue
+        Node *front = queue.front();
+        queue.pop();
+
+        // 2. Do work with the seperated value
+        cout << front->value << " ";
+
+        // 3. Keep the child
+        if (front->left != NULL)
+        {
+            queue.push(front->left);
+        }
+        if (front->right)
+        {
+            queue.push(front->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+
+    // Connections:
+    root->left = a;
+    root->right = b;
+
+    a->left = c;
+    a->right = h;
+
+    c->right = e;
+
+    b->right = d;
+
+    d->left = f;
+    d->right = g;
+
+    h->right = i;
+
+    level_order(root);
+
+    return 0;
+}
